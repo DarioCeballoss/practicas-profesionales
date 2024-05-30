0be45d2c690ee86aa060a91c9adeac3398593083
DarioCeballoss <dario_tec_aut@hotmail.com.ar>
2024-05-30 05:36:33 -0300

    Section

---
+++ album/src/components/cards/Buton.jsx
@@ -0,0 +1,9 @@
+
+export default function Buton(tipo) {
+
+    return (
+        <>
+            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+        </>
+    )
+}
---
+++ album/src/components/views/Section.jsx
@@ -0,0 +1,26 @@
+import Card from '../cards/Card'
+
+export default function Section() {
+
+    return (
+        <>
+
+            <div class="album py-5 bg-body-tertiary">
+                <div class="container">
+
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
